1011be2cb0b5271e8738c5b18b20d754f2206d2f
daheeParkk <qkrekgml7414@naver.com>
2023-06-24 19:14:47 +0900

    feat : 최종 이익을 계산하는 기능 구현

--- src/main/java/blackjack/controller/Blackjack.java
+++ src/main/java/blackjack/controller/Blackjack.java
@@ -2,8 +2,7 @@ package blackjack.controller;
 
 import blackjack.domain.Card;
 import blackjack.domain.Cards;
-
-import java.util.Random;
+import blackjack.domain.Player;
 
 public class Blackjack {
 
@@ -11,5 +10,45 @@ public class Blackjack {
 
     public Card drawCard(int index) {
         return cards.getCard(index);
+    }
+
+    public Player calculateFinalProfit(Player player1, Player player2) {
+        checkMaxResult(player1,player2);
+
+        if (!isCondition(player1)&&!isCondition(player2)) {
+            compareResult(player1,player2);
+        }
+
+        return null;
+    }
+
+    private void checkMaxResult(Player player1, Player player2) {
+        if (isCondition(player1) && isCondition(player2)) {
+            player1.plusProfit();
+            player2.plusProfit();
+        }
+        if (isCondition(player1) && !isCondition(player2)) {
+            player1.plusProfit();
+            player2.minusProfit();
+        }
+        if (isCondition(player2) && !isCondition(player1)) {
+            player2.plusProfit();
+            player1.minusProfit();
+        }
+    }
+
+    private void compareResult(Player player1, Player player2) {
+        if (player1.calculateResult() > player2.calculateResult()) {
+            player1.plusProfit();
+            player2.minusProfit();
+        }
+        if (player1.calculateResult() < player2.calculateResult()) {
+            player2.plusProfit();
+            player1.minusProfit();
+        }
+    }
+
+    private boolean isCondition(Player player) {
+        return player.calculateResult() == 21;
     }
 }
--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,2 +1,34 @@
-package blackjack.domain;public class Player {
+package blackjack.domain;
+
+public class Player {
+    private final String name;
+    private final int bettingAmount;
+    private final PlayerCards myCards = new PlayerCards();
+
+    private int profit;
+
+    public Player(String name, int bettingAmount) {
+        this.name = name;
+        this.bettingAmount = bettingAmount;
+    }
+
+    public void addCard(Card card) {
+        myCards.addCard(card);
+    }
+
+    public int calculateResult() {
+        return myCards.calculateResult();
+    }
+
+    public void plusProfit() {
+        profit = bettingAmount;
+    }
+
+    public void minusProfit() {
+        profit = -bettingAmount;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
 }
