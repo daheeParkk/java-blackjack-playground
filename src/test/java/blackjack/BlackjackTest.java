82d01a1bf61b38c6082bc40eac440b9b071e7805
daheeParkk <qkrekgml7414@naver.com>
2023-06-24 16:38:13 +0900

    test : 카드를 뽑는 함수 테스트

---
+++ src/test/java/blackjack/BlackjackTest.java
@@ -0,0 +1,20 @@
+package blackjack;
+
+import blackjack.controller.Blackjack;
+import blackjack.domain.Card;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BlackjackTest {
+
+    @DisplayName("카드를 뽑는 함수 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1,ONE_DIAMOND","2,TWO_DIAMOND"}, delimiter = ',')
+    public void DrawCard(int index, Card card) {
+        Blackjack blackjack = new Blackjack();
+        assertThat(blackjack.drawCard(index)).isEqualTo(card);
+    }
+}
